41a1a1934db77f7382bf495adcf0b3374fd1d85b
Chris Fahlin <chris.fahlin@tee2.org>
2018-02-14 10:10:29 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/cfahlin/advancedandroid/base/AdvancedApplication.java
@@ -0,0 +1,11 @@
+package com.example.cfahlin.advancedandroid.base;
+
+import android.app.Application;
+
+
+public class AdvancedApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
